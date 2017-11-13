f8b81a9589dade8249f5f4b2763896da8e68ebf1
Sandro Santilli <strk@kbt.io>
2017-11-13 22:26:40 +1300

    Improve test for test for GetLatestCommitTime
    Also check that the latest commit is more recent than the commit
    this PR is based at

--- repo_test.go
+++ repo_test.go
@@ -12,5 +12,11 @@ import (
 
 func TestGetLatestCommitTime(t *testing.T) {
 	lct, _ := GetLatestCommitTime(".")
+	// Time is in the past
 	assert.True(t, lct.Unix() < time.Now().Unix())
+	// Time is after Mon Oct 23 03:52:09 2017 +0300
+	// which is the time of commit
+	// d47b98c44c9a6472e44ab80efe65235e11c6da2a
+	refTime, _ := time.Parse("Mon Jan 02 15:04:05 2006 -0700", "Mon Oct 23 03:52:09 2017 +0300")
+	assert.True(t, lct.Unix() > refTime.Unix())
 }
